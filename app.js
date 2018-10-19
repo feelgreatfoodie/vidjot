bb738ea57b901417ebae92ebc9e3b63ec39c60a7
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-18 19:01:51 -0600

    server side form validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -23,6 +24,10 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars')
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //Index Route
 app.get('/', (req, res) => {
   const title = 'Hola'
@@ -39,6 +44,26 @@ app.get('/about', (req,res) => {
 //Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
+})
+
+app.post('/ideas', (req, res) => {
+  let errors = []
+
+  if(!req.body.title) {
+    errors.push({ text: 'Please add a title' })
+  }
+  if(!req.body.details) {
+    errors.push({ text: 'Please add some details' })
+  }
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  } else {
+    res.send('passed')
+  }
 })
 const port = 5000
 
