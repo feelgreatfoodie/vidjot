35a819e8d81c81b1758fe98a7263b4a9d6f9af74
Christian Bourlier <chrisbourlier@hotmail.com>
2018-10-20 09:45:34 -0600

    deploy heroku

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use('/ideas', ideas)
 app.use('/users', users)
 
 //important to have for deploying to Heroku
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`)
